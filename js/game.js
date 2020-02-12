adb0f98bda248481bf4071c83a06163805b55590
Arian <arian.hoti@hs-augsburg.de>
2020-02-13 00:15:54 +0100

    normalized

--- js/game.js
+++ js/game.js
@@ -196,36 +196,6 @@ document.addEventListener("keydown", event => {
 
   });
 
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-	if (window.AudioContext) {
-		window.audioContext = new window.AudioContext();
-	}
-	var fixAudioContext = function (e) {
-		if (window.audioContext) {
-			// Create empty buffer
-			var buffer = window.audioContext.createBuffer(1, 1, 22050);
-			var source = window.audioContext.createBufferSource();
-			source.buffer = buffer;
-			// Connect to output (speakers)
-			source.connect(window.audioContext.destination);
-			// Play sound
-			if (source.start) {
-				source.start(0);
-			} else if (source.play) {
-				source.play(0);
-			} else if (source.noteOn) {
-				source.noteOn(0);
-			}
-		}
-		// Remove events
-		document.removeEventListener('touchstart', fixAudioContext);
-		document.removeEventListener('touchend', fixAudioContext);
-    };
-    	// iOS 6-8
-	document.addEventListener('touchstart', fixAudioContext);
-	// iOS 9
-	document.addEventListener('touchend', fixAudioContext);
-
 document.addEventListener('touchstart', event => {
     console.log(event.touches[0].clientX)
     if(gameStarted === true){
@@ -245,15 +215,12 @@ document.addEventListener('touchstart', event => {
             text2.style.zIndex = "-1";
         }
     }
-
-    
 },false);
 
 
 document.addEventListener('touchend', event => {
     goingLeft = false;
     goingRight = false;
-
 },false);
 
 window.addEventListener( 'resize', onWindowResize, false );
